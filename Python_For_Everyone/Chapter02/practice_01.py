737a231b3825ed64af8d6fb170ebbeee2ec4c54a
Yoon Young <yunyoung1819@hanmail.net>
2018-07-14 19:59:58 +0900

    chapter 02 practice add

---
+++ Python_For_Everyone/Chapter02/practice_01.py
@@ -0,0 +1,15 @@
+
+#inp = input('Europe floor?')
+#usf = int(inp) + 1      # input 함수는 문자열을 반환한다. 숫자가 아님
+#print('US floor', usf)  # 쉼표는 띄어쓰기 역할을 한다.
+
+# nzt = input('Enter your name: ')
+# print("Hello", nzt)
+
+# 어떤 운영체제는 띄어쓰기를 처리하지 못하기 때문에 파일 이름에는
+# 띄어쓰기 대신 밑줄을 사용하는 것을 권장함
+
+xh = input("Enter Hours: ")
+xr = input("Enter Rate: ")
+xp = float(xh) * float(xr)
+print("Pay:",xp) # 쉼표가 띄어쓰기 역할을 해줌
